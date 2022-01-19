c68b278eef7682eccb7be1c839dc86acebc17f19
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-01-20 04:45:04 +0900

    0120 문서 정리

--- my/tes.py
+++ my/tes.py
@@ -1,13 +1,4 @@
-# 정수 N값 초기화
-N = 0
+def test(a, y):
+    print(a, y)
 
-# 정수 받기
-while 1:
-    N = int(input("1 ~ 1,000 정수 입력: "))
-    if 1 <= N <= 1000:
-        break
-
-# N의 약수 오름차순 출력
-for i in range(1, N+1):
-    if N % i == 0:
-        print(i, end=' ')
+test(5, y=3)
