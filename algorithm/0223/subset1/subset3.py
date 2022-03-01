811b1508464be5f3167dd7922b6370a797982d45
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-03-01 15:11:34 +0900

    subset added

--- algorithm/0223/subset1/subset3.py
+++ algorithm/0223/subset1/subset3.py
@@ -1,27 +1,27 @@
-def subset1(i, N, s, K, rs):
+def subset3(i, N, s, T, sr):
     global cnt
     cnt += 1
-    if s == K:
+    if s == T:
         for j in range(N):
             if bit[j]:
                 print(arr[j], end=' ')
         print()
+    elif s > T:
+        return
+    elif s+sr < T:
+        return
     elif i == N:
-        return
-    elif s > K:
-        return
-    elif s+rs < K:
         return
     else:
         bit[i] = 1
-        subset1(i+1, N, s + arr[i], K, rs-arr[i])
+        subset3(i+1, N, s + arr[i], T, sr - arr[i])
         bit[i] = 0
-        subset1(i+1, N, s, K, rs-arr[i])
+        subset3(i+1, N, s, T, sr - arr[i])
 
 N = 10
-K = 27
+T = 30
 arr = [x for x in range(1, N+1)]
-bit = [0]*N
+bit = [0] * N
 cnt = 0
-subset1(0, N, 0, K, sum(arr))
+subset3(0, N, 0, T, sum(arr))
 print(f'cnt: {cnt}')
