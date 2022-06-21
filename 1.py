5fe739617db61cd3fc83d6ad5eb9a11261cbbb08
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-21 21:24:10 +0900

    lst2 형2

--- 1.py
+++ 1.py
@@ -1,29 +1,3 @@
-def compare(i1, i2):
-    c1, c2 = lst[i1], lst[i2]
-    idx = 0
-    if idx<len(c1) and idx<len(c2):
-        if ord(c1[idx]) > ord(c2[idx]):
-            return
-        elif ord(c1[idx]) < ord(c2[idx]):
-            lst[i1], lst[i2] = c2, c1
-            return
-        idx += 1
+lst = list(map(float, input().split()))
 
-    if len(c1) > len(c2):
-        return
-    else:
-        lst[i1], lst[i2] = c2, c1
-        return
-
-
-lst = []
-
-for _ in range(5):
-    lst.append(input())
-
-for i in range(4, 0, -1):
-    for j in range(i):
-        compare(j, j+1)
-
-for i in range(5):
-    print(lst[i])
+print(f'{sum(lst)/6:.1f}')
