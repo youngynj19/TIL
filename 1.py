1e8c855feb24b3889b5e7b59aae3350caf7636a6
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-21 20:44:42 +0900

    lst 7

---
+++ 1.py
@@ -0,0 +1,33 @@
+def compare(i1, i2):
+    c1, c2 = lst[i1], lst[i2]
+    idx = 0
+    flag = False
+    if idx<len(c1) and idx<len(c2):
+        if ord(c1[idx]) > ord(c2[idx]):
+            flag = True
+        elif ord(c1[idx]) < ord(c2[idx]):
+            lst[i1], lst[i2] = c2, c1
+            return
+        idx += 1
+
+    if len(c1) >= len(c2):
+        return
+    else:
+        if flag:
+            return
+        else:
+            lst[i1], lst[i2] = c2, c1
+            return
+
+
+lst = []
+
+for _ in range(5):
+    lst.append(input())
+
+for i in range(4, 0, -1):
+    for j in range(i):
+        compare(j, j+1)
+
+for i in range(5):
+    print(lst[i])
