dd84afe379fcdf0e0ce16b3dd5eb31d9e877d44b
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 14:45:10 +0900

    lst3 test12

--- 1.py
+++ 1.py
@@ -1,15 +1,5 @@
-n = int(input())
-lst = [[0 for _ in range(n+1)] for _ in range(n)]
-lst[0][1] = 1
-for i in range(1,n):
-    for j in range(1,n+1):
-        lst[i][j] = lst[i-1][j-1] + lst[i-1][j]
-        if lst[i][j] == 0:
-            break
-
-for i in range(n-1,-1, -1):
-    for j in range(1,n+1):
-        if lst[i][j] == 0:
-            break
-        print(lst[i][j],end=' ')
+lst = [input().split() for _ in range(3)]
+for i in range(3):
+    for j in range(5):
+        print(chr(ord(lst[i][j]) + 32), end=' ')
     print()
