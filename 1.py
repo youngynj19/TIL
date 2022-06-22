4a8946e534da414032a0bede18813a8a3ff430d3
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 10:16:51 +0900

    lst3 test4

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
-nums = [0 for _ in range(7)]
-dice = list(map(int, input().split()))
-for n in dice:
-    nums[n] += 1
-for i in range(1,7):
-    print(f'{i} : {nums[i]}')
+scores = list(map(int, input().split()))
+lst = [0 for i in range(11)]
+for score in scores:
+    lst[score//10] += 1
+
+for i in range(10, -1, -1):
+    if lst[i]:
+        print(f'{i*10} : {lst[i]} person')
