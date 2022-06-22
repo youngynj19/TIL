cc574eb03bb2b5ca1e20f41c9b7790242b9d7c21
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 10:34:53 +0900

    lst3 test7

--- 1.py
+++ 1.py
@@ -1,8 +1,9 @@
-lst = [[3, 5, 9],
-
-[2, 11, 5],
-
-[8, 30, 10],
-
-[22, 5, 1]]
-print(sum(sum(lst[i]) for i in range(4)))
+lst = [list((i+1)%2 for i in range(5))] + [[0 for _ in range(5)] for _ in range(4)]
+print(*lst[0])
+for i in range(1,5):
+    for j in range(5):
+        a = lst[i-1][j-1] if 0<=j-1<=4 else 0
+        b = lst[i-1][j+1] if 0<=j+1<=4 else 0
+        lst[i][j] = a+b
+        print(a+b,end=' ')
+    print()
