5b59affe16e2d47ed6f102db2f6a8360543ebbd8
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 14:51:33 +0900

    other self4

--- 1.py
+++ 1.py
@@ -1,5 +1,6 @@
-lst = [input().split() for _ in range(3)]
+lst = []
 for i in range(3):
-    for j in range(5):
-        print(chr(ord(lst[i][j]) + 32), end=' ')
-    print()
+    lst.append(list(map(int, input().split())))
+    lst[i] = set(lst[i])
+print(f'Intersection: {lst[0]&lst[1]&lst[2]}')
+print(f'Union: {lst[0]|lst[1]|lst[2]}')
