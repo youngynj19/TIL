318d98dba88e17b71ea08356bc0317259c98e7a2
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 10:24:17 +0900

    lst3 test5

--- 1.py
+++ 1.py
@@ -1,8 +1,4 @@
-scores = list(map(int, input().split()))
-lst = [0 for i in range(11)]
-for score in scores:
-    lst[score//10] += 1
-
-for i in range(10, -1, -1):
-    if lst[i]:
-        print(f'{i*10} : {lst[i]} person')
+lst = list(map(int, input().split()))
+for i in range(2, 10):
+    lst.append((lst[i-1] + lst[i-2])%10)
+print(*lst)
