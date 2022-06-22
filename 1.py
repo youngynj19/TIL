68e184e6f1c6dfaf4c23622ec4f81e335ef4048c
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 21:35:37 +0900

    others test3

--- 1.py
+++ 1.py
@@ -1,13 +1,21 @@
+def compare(i1, i2):
+    c1, c2 = lst[i1], lst[i2]
+    i = 0
+    while i<len(c1) and i<len(c2):
+        if ord(c1[i]) < ord(c2[i]):
+            return
+        elif ord(c2[i]) < ord(c1[i]):
+            lst[i1], lst[i2] = c2, c1
+        i += 1
+    if len(c1) > len(c2):
+        lst[i1], lst[i2] = c2, c1
+
 lst = list(input().split())
-n = int(input())
-dic = dict()
-for name in lst:
-    if name in dic:
-        dic[name] += 1
-    else:
-        dic.update({name: 1})
-for name in lst:
-    a = dic.get(name)
-    if a == n:
-        print(name)
-        dic.pop(name)
+s = set(lst)
+lst = list(s)
+l = len(lst)
+for i in range(l-1, 0, -1):
+    for j in range(i):
+        compare(j, j+1)
+print(*lst)
+print(l)
