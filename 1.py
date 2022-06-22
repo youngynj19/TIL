49ae0f691d7251dc408d4bf717c7b9714b296ed4
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-22 10:12:18 +0900

    lst3 test3

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
-size, n = map(int, input().split())
-lst = [i%n==0 for i in range(size)]
-print(lst)
+nums = [0 for _ in range(7)]
+dice = list(map(int, input().split()))
+for n in dice:
+    nums[n] += 1
+for i in range(1,7):
+    print(f'{i} : {nums[i]}')
