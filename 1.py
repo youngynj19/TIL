c589ad2f4549d055a67c58cea78a6795d88235bd
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-25 21:20:17 +0900

    f1 self6

--- 1.py
+++ 1.py
@@ -1,9 +1,14 @@
-def f(n, m):
-    ans = 1
-    for _ in range(m):
-        ans *= n
-    return ans
+def f(lst):
+    n1 = int(lst[0])
+    n2 = int(lst[2])
+    opr = lst[1]
+    opr_lst = ['+', '-', '*', '/']
+    ans_lst = [n1+n2, n1-n2, n1*n2, n1//n2]
+    for i in range(4):
+        if opr == opr_lst[i]:
+            return ans_lst[i]
+    return 0
 
-n, m = map(int, input().split())
+lst = list(input().split())
 
-print(f(n, m))
+print(*lst, f'= {f(lst)}')
