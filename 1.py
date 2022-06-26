6e48609fc18565cae252feae7dd11782815da5b4
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-26 21:24:34 +0900

    f1 test3

--- 1.py
+++ 1.py
@@ -1,26 +1,7 @@
-'''
-== 3dan ==
-3 * 1 =  3
-3 * 2 =  6
-3 * 3 =  9
-3 * 4 = 12
-3 * 5 = 15
-3 * 6 = 18
-3 * 7 = 21
-3 * 8 = 24
-3 * 9 = 27
-'''
-
-def f(n1, n2):
-    if n2 < n1:
-        n1, n2 = n2, n1
-
-    for n in range(n1, n2 + 1):
-        print(f'== {n}dan ==')
-        for i in range(1,10):
-            print(f'{n} * {i} = {n*i:>2}')
+def f(n):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            print(i*j, end=' ')
         print()
 
-n1, n2 = map(int, input().split())
-
-f(n1, n2)
+f(int(input()))
