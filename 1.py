c7e93c0161fdc80df9d7487ae0f74d153c1bffa0
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-26 18:53:27 +0900

    f1 test5

--- 1.py
+++ 1.py
@@ -1,14 +1,26 @@
-def f(lst):
-    n1 = int(lst[0])
-    n2 = int(lst[2])
-    opr = lst[1]
-    opr_lst = ['+', '-', '*', '/']
-    ans_lst = [n1+n2, n1-n2, n1*n2, n1//n2]
-    for i in range(4):
-        if opr == opr_lst[i]:
-            return ans_lst[i]
-    return 0
+'''
+== 3dan ==
+3 * 1 =  3
+3 * 2 =  6
+3 * 3 =  9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21
+3 * 8 = 24
+3 * 9 = 27
+'''
 
-lst = list(input().split())
+def f(n1, n2):
+    if n2 < n1:
+        n1, n2 = n2, n1
 
-print(*lst, f'= {f(lst)}')
+    for n in range(n1, n2 + 1):
+        print(f'== {n}dan ==')
+        for i in range(1,10):
+            print(f'{n} * {i} = {n*i:>2}')
+        print()
+
+n1, n2 = map(int, input().split())
+
+f(n1, n2)
