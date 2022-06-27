6895ff6d6206ced7d5a6e9b4d35685bd68a0d3df
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-27 23:11:02 +0900

    f2 self1

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
-def f(n):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            print(i*j, end=' ')
-        print()
+def f(n, lst):
+    for i in range(n-1, 0, -1):
+        for j in range(i):
+            if lst[j] < lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    print(*lst)
 
-f(int(input()))
+n = int(input())
+lst = list(map(int, input().split()))
+f(n, lst)
