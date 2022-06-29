6aee58707529aa493997d23a11062917d99d743f
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-29 20:08:04 +0900

    f2 self4

--- 1.py
+++ 1.py
@@ -1,16 +1,3 @@
-def f1(a1, a2):
-    if abs(a1) > abs(a2):
-        return a1
-    else:
-        return a2
-
-def f2(a, b):
-    if abs(a) < abs(b):
-        return a
-    else:
-        return b
-
-a, b = map(int, input().split())
-print(f1(a, b))
-c,d = map(float, input().split())
-print(f2(c, d))
+def radius(area):
+    print(f'{(area/3.14)**(1/2):.2f}')
+radius(int(input()))
