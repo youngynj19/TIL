14e1a1f0576cc66a9ac50760f67bcfcdb3e24c37
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-29 21:24:16 +0900

    bubble sort

--- 1.py
+++ 1.py
@@ -1,34 +1,9 @@
-def f(a,b,c):
-    mx,md,mn = a,a,a
+def bubble(lst):
+    l = len(lst)
+    for i in range(l-1,0,-1):
+        for j in range(i):
+            if lst[j] < lst[j+1]:
+                lst[j],lst[j+1] = lst[j+1],lst[j]
+        print(*lst)
 
-    if b > a:
-        mx = b
-    else:
-        md = b
-
-    if c>a and c>b:
-        mx,md,mn = c,mx,md
-    elif c>a or c>b:
-        md,mn = c, md
-    else:
-        mn = c
-
-    print(int(mx*10//10+1),int(mn*10//10),int((md+0.5)*10//10))
-
-a,b,c = map(float, input().split())
-f(a,b,c)
-
-# mx, mn, md = 0,0,0
-# if a >= b:
-#     if c>=a:
-#         mx, md, mn = c,a,b
-#     elif b>=c:
-#         mx, md, mn = a,b,c
-#     else:
-#         mx,md,mn = a,c,b
-# elif c>=b:
-#     mx, md,mn = c,b,a
-# elif a>=c:
-#     mx,md,mn = b,a,c
-# else:
-#     mx,md,mn = b,c,a
+bubble(list(map(int, input().split())))
