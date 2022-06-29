dd2664bcd4bc080f1e8028adf6ae646e4b224935
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-06-29 22:30:06 +0900

    f3 self4

--- 1.py
+++ 1.py
@@ -1,6 +1,12 @@
-def f(n):
-    if n == 0:
-        return 0
-    return n+f(n-1)
+def roll(lev, num):
+    global L
+    if lev == L:
+        print(*dice)
+        return
+    for n in range(num,7):
+        dice[lev] = n
+        roll(lev+1, n)
 
-print(f(int(input())))
+L = int(input())
+dice = [0] * L
+roll(0, 1)
