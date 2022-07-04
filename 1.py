90c625f16625cea3844328f093991299d4f741d0
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-07-04 09:57:12 +0900

    f3 test1

--- 1.py
+++ 1.py
@@ -1,7 +1,11 @@
+lst = []
 def f(n):
-    if n < 10:
-        return n**2
+    if n == 1:
+        lst.append(n)
+        return
+    
+    lst.append(n)
+    f(n//2)
 
-    return (n%10)**2 + f(n//10)
-
-print(f(int(input())))
+f(int(input()))
+print(*lst[::-1])
