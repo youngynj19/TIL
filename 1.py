09c6995c9959cd4b16375ea7cc8a6ce125232904
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-08-02 02:08:52 +0900

    f3 test4

--- 1.py
+++ 1.py
@@ -1,7 +1,16 @@
-def f(n):
-    if n <= 0:
-        return
-    f(n-2)
-    print(n, end=' ')
+N = int(input())
+path = [0]*101
 
-f(int(input()))
+def f(n):
+    if path[n] > 0:
+        return path[n]
+    
+    if n == 1:
+        path[1] = 1
+    elif n == 2:
+        path[2] = 2
+    else:
+        path[n] = f(n-2)*f(n-1)%100
+    return path[n]
+
+print(f(N))
